9485d832ee82245ddb29f58996d4055c958da260
oliver ahmed <oliverahmed000@gmail.com>
2026-07-23 11:32:52 +0600

    merge conflict fixed

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('test_token', function (Request $request) {
-    return response()->json([
-        'raw_cookie_header' => $request->header('Cookie'),
-        'parsed_cookie_value' => $request->cookie('access_token'),
-    ]);
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -32,13 +24,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 |--------------------------------------------------------------------------
 */
 Route::middleware('auth:sanctum')->group(function () {
-<<<<<<< HEAD
-=======
-
 
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
->>>>>>> 3b21b59f4a9cf7d24971ce885aef9cbe15150853
 
     // Authenticated Write Actions (Create, Update, Delete)
     Route::apiResource('categories', CategoryController::class)->except(['index', 'show']);
@@ -46,7 +34,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // Orders are fully private (a user must be logged in to buy or see history)
     Route::apiResource('orders', OrderController::class);
-
-
 
 });
