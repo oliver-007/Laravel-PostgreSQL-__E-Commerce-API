5aea6cc53245bf21d5120369c26cc30c1c564047
oliver ahmed <oliverahmed000@gmail.com>
2026-07-23 17:43:55 +0600

    bug fixed in destroy() in PorductController & add Rule in ProductRequest name

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ProductRequest extends FormRequest
 {
@@ -21,6 +22,8 @@ class ProductRequest extends FormRequest
      */
     public function rules(): array
     {
+     
+       $product =  $this->route('product');
         return [
             'category_id' => [
                 'required',
@@ -30,6 +33,7 @@ class ProductRequest extends FormRequest
                 'required',
                 'string',
                 'max:255',
+                Rule::unique('products', 'name')->ignore($product ? $product->id : null),
             ],
             'price' => [
                 'required',
