752a63bacdae54b8e664b44cd7a51285c0bb367c
Oliver Ahmed <55628096+oliver-007@users.noreply.github.com>
2026-09-09 18:15:42 +0600

    extended Controllers added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
-class UserController
+class UserController extends Controllers
 {
     /**
      * Display a listing of the resource.
